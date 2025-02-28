332d59b0e003bea43a0b7d90944668fabb1e634d
Mhamed Azirar <azirarm@gmail.com>
2025-02-28 12:04:22 +0100

    add xml content in codes

--- itexpert-content-lib/src/main/java/com/itexpert/content/lib/enums/ContentTypeEnum.java
+++ itexpert-content-lib/src/main/java/com/itexpert/content/lib/enums/ContentTypeEnum.java
@@ -1,5 +1,5 @@
 package com.itexpert.content.lib.enums;
 
 public enum ContentTypeEnum {
-    FILE, PICTURE, SCRIPT, STYLE, HTML, JSON, URLS, DATA
+    FILE, PICTURE, SCRIPT, STYLE, HTML, JSON, URLS, DATA, XML
 }
