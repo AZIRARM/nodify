141f0d50ea4785f66e59be327f1dcddc2e3e92d6
Mhamed.Azirar <mhamed.azirar@sncf.fr>
2025-03-27 12:45:40 +0100

    resolving minor bug on display content code

--- itexpert-content-ui/src/app/components/content-node/content-code/content-code-js/content-code-js.component.ts
+++ itexpert-content-ui/src/app/components/content-node/content-code/content-code-js/content-code-js.component.ts
@@ -1,14 +1,26 @@
-import {Component, Input, Output} from '@angular/core';
+import {AfterViewInit, Component, Input, Output, ViewChild} from '@angular/core';
 import {ContentNode} from "../../../../modeles/ContentNode";
+import {CodemirrorComponent} from "@ctrl/ngx-codemirror";
 
 @Component({
   selector: 'app-content-code-js',
   templateUrl: './content-code-js.component.html',
   styleUrl: './content-code-js.component.css'
 })
-export class ContentCodeJsComponent {
+export class ContentCodeJsComponent implements AfterViewInit{
 
   @Output()
   @Input()
   contentNode: ContentNode;
+
+  @ViewChild(CodemirrorComponent) codeMirrorComponent!: CodemirrorComponent;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.codeMirrorComponent?.codeMirror) {
+        this.codeMirrorComponent.codeMirror.refresh();
+      }
+    }, 300);
+  }
+
 }
--- itexpert-content-ui/src/app/components/content-node/content-code/content-code-xml/content-code-xml.component.ts
+++ itexpert-content-ui/src/app/components/content-node/content-code/content-code-xml/content-code-xml.component.ts
@@ -1,14 +1,25 @@
-import {Component, Input, Output} from '@angular/core';
+import {AfterViewInit, Component, Input, Output, ViewChild} from '@angular/core';
 import {ContentNode} from "../../../../modeles/ContentNode";
+import {CodemirrorComponent} from "@ctrl/ngx-codemirror";
 
 @Component({
   selector: 'app-content-code-xml',
   templateUrl: './content-code-xml.component.html',
   styleUrl: './content-code-xml.component.css'
 })
-export class ContentCodeXmlComponent {
+export class ContentCodeXmlComponent implements AfterViewInit{
 
   @Output()
   @Input()
   contentNode: ContentNode;
+
+  @ViewChild(CodemirrorComponent) codeMirrorComponent!: CodemirrorComponent;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.codeMirrorComponent?.codeMirror) {
+        this.codeMirrorComponent.codeMirror.refresh();
+      }
+    }, 300);
+  }
 }
