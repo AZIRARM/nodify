e1056489dd7ac03204f1efdb2dc4f2cab5d91ae1
Mhamed.Azirar <mhamed.azirar@sncf.fr>
2025-03-27 12:45:40 +0100

    resolving minor bug on display content code

--- itexpert-content-ui/src/app/components/content-node/content-code/content-code-json/content-code-json.component.ts
+++ itexpert-content-ui/src/app/components/content-node/content-code/content-code-json/content-code-json.component.ts
@@ -1,14 +1,25 @@
-import {Component, Input, Output} from '@angular/core';
+import {AfterViewInit, Component, Input, Output, ViewChild} from '@angular/core';
 import {ContentNode} from "../../../../modeles/ContentNode";
+import {CodemirrorComponent} from "@ctrl/ngx-codemirror";
 
 @Component({
   selector: 'app-content-code-json',
   templateUrl: './content-code-json.component.html',
   styleUrl: './content-code-json.component.css'
 })
-export class ContentCodeJsonComponent {
+export class ContentCodeJsonComponent implements AfterViewInit{
 
   @Output()
   @Input()
   contentNode: ContentNode;
+
+  @ViewChild(CodemirrorComponent) codeMirrorComponent!: CodemirrorComponent;
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      if (this.codeMirrorComponent?.codeMirror) {
+        this.codeMirrorComponent.codeMirror.refresh();
+      }
+    }, 300);
+  }
 }
