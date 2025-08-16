19ff3eb945db1ab10023ca83e73a1125cbd53205
MammothDevelopper <azirarm@gmail.com>
2025-08-16 21:15:55 +0200

    fix: refact tests

--- itexpert-content-core/src/main/java/com/itexpert/content/core/helpers/ContentNodeSlugHelper.java
+++ itexpert-content-core/src/main/java/com/itexpert/content/core/helpers/ContentNodeSlugHelper.java
@@ -2,6 +2,7 @@ package com.itexpert.content.core.helpers;
 
 import com.itexpert.content.core.repositories.ContentNodeRepository;
 import com.itexpert.content.core.repositories.NodeRepository;
+import com.itexpert.content.core.utils.SlugsUtils;
 import com.itexpert.content.lib.models.ContentNode;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,7 +20,7 @@ public class ContentNodeSlugHelper {
     private final NodeRepository nodeRepository;
 
     public Mono<ContentNode> update(ContentNode content, String environment) {
-        return this.renameSlug(content, environment, this.generateSlug(content.getSlug(), environment, 0));
+        return this.renameSlug(content, environment, SlugsUtils.generateSlug(content.getSlug(), environment, 0));
     }
 
     private Mono<ContentNode> renameSlug(ContentNode content, String environment, String slug) {
@@ -31,20 +32,20 @@ public class ContentNodeSlugHelper {
                 .hasElements() // transforme le Flux en Mono<Boolean>
                 .flatMap(exists -> {
                     if (exists) {
-                       return Mono.just(content);
+                        return Mono.just(content);
                     } else {
                         return this.contentNodeRepository.findAllBySlug(slug)
                                 .hasElements()
                                 .flatMap(existsInContentNodeRepo -> {
                                     if (existsInContentNodeRepo) {
-                                        String newSlug = this.generateSlug(content.getSlug(), environment, extractRec(slug) + 1);
+                                        String newSlug = SlugsUtils.generateSlug(content.getSlug(), environment, SlugsUtils.extractRec(slug) + 1);
                                         return renameSlug(content, environment, newSlug);
                                     } else {
                                         return this.nodeRepository.findAllBySlug(slug)
                                                 .hasElements()
                                                 .flatMap(existsInNodeRepo -> {
                                                     if (existsInNodeRepo) {
-                                                        String newSlug = this.generateSlug(content.getSlug(), environment, extractRec(slug) + 1);
+                                                        String newSlug = SlugsUtils.generateSlug(content.getSlug(), environment, SlugsUtils.extractRec(slug) + 1);
                                                         return renameSlug(content, environment, newSlug);
                                                     } else {
                                                         content.setSlug(slug);
@@ -56,24 +57,4 @@ public class ContentNodeSlugHelper {
                     }
                 });
     }
-
-    private String generateSlug(String slug, String environment, int rec) {
-        if (ObjectUtils.isNotEmpty(slug)) {
-            String baseSlug = ObjectUtils.isNotEmpty(environment) ? slug.trim().replace(environment.toLowerCase(), "") :  slug.trim();
-            if (rec <= 0) {
-                return ObjectUtils.isNotEmpty(environment) ? baseSlug + "-" + environment.toLowerCase() : baseSlug;
-            } else {
-                return ObjectUtils.isNotEmpty(environment) ? baseSlug + "-" + environment.toLowerCase() + rec :  baseSlug + "-" + rec;
-            }
-        }
-        return null;
-    }
-
-    // Permet de récupérer le compteur rec d'un slug existant
-    private int extractRec(String slug) {
-        String digits = slug.replaceAll(".*?(\\d+)$", "$1");
-        return digits.matches("\\d+") ? Integer.parseInt(digits) : 0;
-    }
-
-
 }
