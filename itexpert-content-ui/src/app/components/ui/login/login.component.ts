0431ba18b28eee56f3a206362453a6fa42b26f0a
MammothDevelopper <azirarm@gmail.com>
2025-08-21 22:43:13 +0200

    logs login success and error

--- itexpert-content-ui/src/app/components/ui/login/login.component.ts
+++ itexpert-content-ui/src/app/components/ui/login/login.component.ts
@@ -5,6 +5,8 @@ import {AuthenticationService} from "../../../services/AuthenticationService";
 import {UserAccessService} from "../../../services/UserAccessService";
 import { CookiesService } from 'src/app/services/CookiesService';
 import { UserService } from 'src/app/services/UserService';
+import {LoggerService} from "../../../services/LoggerService";
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-login',
@@ -17,6 +19,8 @@ export class LoginComponent {
   constructor(private authenticationService: AuthenticationService,
               private cookiesService: CookiesService,
               private userService: UserService,
+              private loggerService: LoggerService,
+              private translate: TranslateService,
               private router: Router) {
 
   }
@@ -25,9 +29,16 @@ export class LoginComponent {
     if (this.userLogin.email && this.userLogin.password) {
       this.authenticationService.signin(this.userLogin)
         .subscribe((response) => {
+              this.translate.get("LOGIN_SUCCESS").subscribe(trad => {
+                this.loggerService.success(trad);
+              });
               this.cookiesService.setCookie("userToken", JSON.stringify(response), 1);
               this.authenticationService.loadUser();
               this.router.navigateByUrl('/nodes');
+          }, error => {
+          this.translate.get("LOGIN_ERROR").subscribe(trad => {
+            this.loggerService.error(trad);
+          });
           }
         );
     }
