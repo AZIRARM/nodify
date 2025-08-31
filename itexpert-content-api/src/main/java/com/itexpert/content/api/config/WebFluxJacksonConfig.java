a27c5a70db0e4e46b87208ffaa19a533f68ef892
AZIRAR Mhamed (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDSP IDFCE) <7812170p@commun.ad.sncf.fr>
2025-08-31 14:17:40 +0200

    add conf to accept big file max 50Mo

---
+++ itexpert-content-api/src/main/java/com/itexpert/content/api/config/WebFluxJacksonConfig.java
@@ -0,0 +1,35 @@
+package com.itexpert.content.api.config;
+
+import com.fasterxml.jackson.core.StreamReadConstraints;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.http.codec.json.Jackson2JsonDecoder;
+import org.springframework.http.codec.json.Jackson2JsonEncoder;
+
+@Configuration
+public class WebFluxJacksonConfig {
+
+    @Bean
+    public ServerCodecConfigurer serverCodecConfigurer(ObjectMapper mapper) {
+        // augmenter la limite Jackson (string base64)
+        mapper.getFactory().setStreamReadConstraints(
+                StreamReadConstraints.builder()
+                        .maxStringLength(50_000_000) // 50MB au lieu de 5MB
+                        .build()
+        );
+
+        // augmenter la limite de buffer WebFlux
+        Jackson2JsonDecoder decoder = new Jackson2JsonDecoder(mapper);
+        decoder.setMaxInMemorySize(50 * 1024 * 1024); // 50MB
+
+        Jackson2JsonEncoder encoder = new Jackson2JsonEncoder(mapper);
+
+        ServerCodecConfigurer configurer = ServerCodecConfigurer.create();
+        configurer.defaultCodecs().jackson2JsonDecoder(decoder);
+        configurer.defaultCodecs().jackson2JsonEncoder(encoder);
+
+        return configurer;
+    }
+}
