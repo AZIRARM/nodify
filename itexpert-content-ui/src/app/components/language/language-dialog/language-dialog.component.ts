3847faa5d4bb60b648b2027d721228c1eaca6962
MammothDevelopper <azirarm@gmail.com>
2026-04-18 12:55:08 +0200

    feat/angular-upgrade-19-plus: migrate notifications

--- itexpert-content-ui/src/app/components/language/language-dialog/language-dialog.component.ts
+++ itexpert-content-ui/src/app/components/language/language-dialog/language-dialog.component.ts
@@ -1,29 +1,38 @@
-import {Component, Inject} from '@angular/core';
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {Language} from "../../../modeles/Language";
+import { Component, inject, signal, computed } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Language } from "../../../modeles/Language";
 
 @Component({
-    selector: 'app-language-dialog',
-    templateUrl: './language-dialog.component.html',
-    styleUrls: ['./language-dialog.component.css'],
-    standalone: false
+  selector: 'app-language-dialog',
+  templateUrl: './language-dialog.component.html',
+  styleUrls: ['./language-dialog.component.css'],
+  standalone: false
 })
 export class LanguageDialogComponent {
 
-  language: Language = new Language();
+  private dialogRef = inject(MatDialogRef<LanguageDialogComponent>);
+  private content = inject<Language | null>(MAT_DIALOG_DATA);
 
-  constructor(
-    public dialogRef: MatDialogRef<LanguageDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public content: Language
-  ) {
-    if (content) {
-      // Créer une copie pour ne pas modifier l'original directement
-      this.language = {...content};
-    }
+  language = signal<Language>(this.content ? { ...this.content } : new Language());
+
+  isFormValid = computed(() =>
+    !!(this.language().code && this.language().name)
+  );
+
+  updateCode(code: string) {
+    this.language.update(l => ({ ...l, code }));
   }
 
-  isFormValid(): boolean {
-    return !!(this.language.code && this.language.name);
+  updateName(name: string) {
+    this.language.update(l => ({ ...l, name }));
+  }
+
+  updateDescription(description: string) {
+    this.language.update(l => ({ ...l, description }));
+  }
+
+  updateUrlIcon(urlIcon: string) {
+    this.language.update(l => ({ ...l, urlIcon }));
   }
 
   cancel() {
@@ -32,7 +41,7 @@ export class LanguageDialogComponent {
 
   validate() {
     if (this.isFormValid()) {
-      this.dialogRef.close({data: this.language});
+      this.dialogRef.close({ data: this.language() });
     }
   }
 }
